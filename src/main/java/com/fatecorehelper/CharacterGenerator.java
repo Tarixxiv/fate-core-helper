fee57c8622bf7b960a87594a28da759719dcd763
Tarixxiv <r.litwin@interia.pl>
2022-08-10 22:40:37 +0200

    added regenerate button regenerating aspects added checkbox disabling and enabling aspect fields

--- src/main/java/com/fatecorehelper/CharacterGenerator.java
+++ src/main/java/com/fatecorehelper/CharacterGenerator.java
@@ -3,6 +3,7 @@ package com.fatecorehelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -17,12 +18,25 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CharacterGenerator extends Application {
     AspectRandomizer aspectRandomizer = new AspectRandomizer();
     SkillPointDistributor skillPointDistributor = new SkillPointDistributor();
 
+    ArrayList<CheckBox> aspectCheckboxes = new ArrayList<>();
+    ArrayList<TextField> aspectFields = new ArrayList<>();
+
     public CharacterGenerator() {
+    }
+
+    public void regenerate() throws FileNotFoundException {
+        ArrayList<String> aspects = aspectRandomizer.generateAspects();
+        for (int i = 0; i < aspectCheckboxes.size(); i++) {
+            if (!aspectCheckboxes.get(i).isSelected()){
+                aspectFields.get(i).setText(aspects.get(i));
+            }
+        }
     }
 
     @Override
@@ -34,7 +48,17 @@ public class CharacterGenerator extends Application {
             HBox hbox = new HBox(8);
             TextField textField = new TextField(aspect);
             textField.setPrefWidth(400);
-            hbox.getChildren().addAll(new CheckBox(),textField);
+            CheckBox checkBox = new CheckBox();
+            checkBox.setOnAction(event -> {
+                if (checkBox.isSelected()){
+                    textField.setDisable(true);
+                }else{
+                    textField.setDisable(false);
+                }
+            });
+            hbox.getChildren().addAll(checkBox,textField);
+            aspectCheckboxes.add(checkBox);
+            aspectFields.add(textField);
             vbox.getChildren().add(hbox);
         }
         skillPointDistributor.distributeSkillPoints();
@@ -62,6 +86,13 @@ public class CharacterGenerator extends Application {
         vbox.getChildren().add(grid);
         vbox.getChildren().add(new Label("SP left after generation : " + skillPointDistributor.skillPointsLeft));
         Button button = new Button("Generate");
+        button.setOnAction(event -> {
+            try {
+                regenerate();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
         vbox.getChildren().add(button);
         vbox.setPadding(new Insets(30));
         Scene scene = new Scene(vbox, 1000,600);

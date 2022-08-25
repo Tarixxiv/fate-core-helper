4944769ee43232de1e75d294b470902a80a10737
Tarixxiv <r.litwin@interia.pl>
2022-08-25 16:28:33 +0200

    added skill regeneration using button

--- src/main/java/com/fatecorehelper/CharacterGenerator.java
+++ src/main/java/com/fatecorehelper/CharacterGenerator.java
@@ -12,7 +12,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -22,7 +21,7 @@ public class CharacterGenerator extends Application {
     Label skillPointsLeftLabel = new Label();
     ArrayList<CheckBox> aspectCheckboxes = new ArrayList<>();
     ArrayList<TextField> aspectFields = new ArrayList<>();
-    ArrayList<SkillColumn> skillColumns = new ArrayList<>();
+    ArrayList<SkillColumn> skillGrid = new ArrayList<>();
     ArrayList<String> disabledAspectTextFieldInput = new ArrayList<>();
 
     ArrayList<String> disabledSkillTextFieldInput = new ArrayList<>();
@@ -30,13 +29,21 @@ public class CharacterGenerator extends Application {
     public CharacterGenerator() {
     }
 
-    public void generateCharacter() throws FileNotFoundException {
+    private void parsePyramid() throws IOException {
+        skillPointDistributor.distributeSkillPoints();
+        for (int i = 0; i < skillPointDistributor.pyramidWidth; i++) {
+            skillGrid.get(i).parseSkills(skillPointDistributor.skillPyramid.get(i));
+        }
+    }
+
+    public void generateCharacter() throws IOException {
         ArrayList<String> aspects = aspectRandomizer.generateAspects(disabledAspectTextFieldInput);
         for (int i = 0; i < aspectCheckboxes.size(); i++) {
             if (!aspectCheckboxes.get(i).isSelected()){
                 aspectFields.get(i).setText(aspects.get(i));
             }
         }
+        parsePyramid();
     }
 
     void createAspectFieldsAndCheckboxes(VBox vbox){
@@ -60,8 +67,10 @@ public class CharacterGenerator extends Application {
         }
     }
 
-    void createSkillFieldsAndCheckBoxes(VBox vbox) throws IOException {
-        skillPointDistributor.distributeSkillPoints();
+
+
+    void createSkillFieldsAndCheckBoxes(VBox vbox) {
+
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(5);
@@ -72,33 +81,22 @@ public class CharacterGenerator extends Application {
             grid.add(label,0,i + 1);
         }
         for (int i = 0; i < skillPointDistributor.pyramidWidth; i++) {
-            ArrayList<TextField> skillColumn = new ArrayList<>();
+            ArrayList<TextField> textFieldsColumn = new ArrayList<>();
             CheckBox checkBox = new CheckBox();
-            SkillColumn columnCheckbox = new SkillColumn(skillColumn,checkBox);
-            skillColumns.add(columnCheckbox);
+            SkillColumn skillColumn = new SkillColumn(textFieldsColumn,checkBox);
+            skillGrid.add(skillColumn);
             grid.add(checkBox,i + 1,0);
             for (int j = 0; j < maxPyramidHeight; j++) {
                 HBox hbox = new HBox(8);
-                String text = "";
-                if (skillPointDistributor.skillPyramid.get(i).size() > maxPyramidHeight - j - 1){
-                    text = skillPointDistributor.skillPyramid.get(i).get(maxPyramidHeight - j - 1);
-                }
-                TextField textField = new TextField(text);
-                skillColumn.add(textField);
+
+                TextField textField = new TextField();
+                textFieldsColumn.add(textField);
                 hbox.getChildren().addAll(textField);
                 grid.add(hbox,i + 1,j + 1);
             }
         }
 
-/*        for (int i = 0; i < skillPointDistributor.pyramidWidth; i++) {
-            for (int j = 0; j < maxPyramidHeight; j++) {
-                String text = "";
-                if (skillPointDistributor.skillPyramid.get(i).size() > maxPyramidHeight - j - 1){
-                    text = skillPointDistributor.skillPyramid.get(i).get(maxPyramidHeight - j - 1);
-                }
 
-            }
-        }*/
 
         vbox.getChildren().add(grid);
         skillPointsLeftLabel.setText("SP left after generation : " + skillPointDistributor.skillPointsLeft);
@@ -109,14 +107,15 @@ public class CharacterGenerator extends Application {
     public void start(Stage stage) throws IOException {
         VBox vbox = new VBox(8);
         createAspectFieldsAndCheckboxes(vbox);
-        generateCharacter();
         createSkillFieldsAndCheckBoxes(vbox);
+        generateCharacter();
+
 
         Button button = new Button("Generate");
         button.setOnAction(event -> {
             try {
                 generateCharacter();
-            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         });
--- src/main/java/com/fatecorehelper/SkillColumn.java
+++ src/main/java/com/fatecorehelper/SkillColumn.java
@@ -6,23 +6,29 @@ import javafx.scene.control.TextField;
 import java.util.ArrayList;
 
 public class SkillColumn {
-    private final ArrayList<TextField> skillColumn;
+    private final ArrayList<TextField> textFields;
     private final CheckBox checkBox;
 
-    SkillColumn(ArrayList<TextField> skillColumn, CheckBox checkBox){
-        this.skillColumn = skillColumn;
+    SkillColumn(ArrayList<TextField> textFields, CheckBox checkBox){
+        this.textFields = textFields;
         checkBox.setOnAction(event -> {
             for (TextField textField:
-                    skillColumn) {
+                    textFields) {
                 textField.setDisable(checkBox.isSelected());
             }
         });
         this.checkBox = checkBox;
     }
 
+    public void parseSkills(ArrayList<String> arrayList){
+        for (int i = 0; i < arrayList.size(); i++) {
+            textFields.get(textFields.size() - i - 1).setText(arrayList.get(i));
+        }
+    }
+
     public void clear(){
         for (TextField textField:
-                skillColumn) {
+                textFields) {
             textField.setText("");
         }
     }
@@ -33,9 +39,9 @@ public class SkillColumn {
 
     public int getSkillPointsInColumn(){
         int output = 0;
-        for (int i = 0; i < skillColumn.size(); i++) {
-            if (!skillColumn.get(i).getText().isBlank()){
-                output += (skillColumn.size() - i);
+        for (int i = 0; i < textFields.size(); i++) {
+            if (!textFields.get(i).getText().isBlank()){
+                output += (textFields.size() - i);
             }
         }
         return output;
@@ -44,7 +50,7 @@ public class SkillColumn {
     public ArrayList<String> getSkills() {
         ArrayList<String> output = new ArrayList<>();
         for (TextField textField:
-                skillColumn){
+                textFields){
             if (!textField.getText().isBlank()){
                 output.add(textField.getText());
             }
